863d645f5dc6d6fd8bcde3ea8cdb5fbde947dbef
hytahie24 <124709997+hytahie24@users.noreply.github.com>
2024-05-02 23:04:30 -0400

    Update add-single-row.mjs

--- components/google_sheets/actions/add-single-row/add-single-row.mjs
+++ components/google_sheets/actions/add-single-row/add-single-row.mjs
@@ -73,48 +73,23 @@ export default {
   },
   async run({ $ }) {
     const sheetId = this.sheetId?.value || this.sheetId;
-    let cells;
+    let row;
     if (this.hasHeaders === "Yes") {
-      // TODO: If we could create a variable using this.allColumns in additionalProps, we dont need
-      // to call getSpreadsheetValues here again.
-      const { values: rows } = await this.googleSheets.getSpreadsheetValues(sheetId, `${this.sheetName}!1:1`);
-      const [
-        headers,
-      ] = rows;
-      cells = headers
-        .map((_, i) => `col_${i.toString().padStart(4, "0")}`)
-        .map((column) => this[column] ?? "");
+      row = {
+        "Time": new Date().toISOString(),
+        "State": "Alarmed",
+      };
     } else {
-      cells = this.googleSheets.sanitizedArray(this.myColumnData);
+      row = ["Alarmed", new Date().toISOString()];
     }
-
-    // validate input
-    if (!cells || !cells.length) {
-      throw new Error("Please enter an array of elements in `Cells / Column Values`.");
-    } else if (!Array.isArray(cells)) {
-      throw new Error("Cell / Column data is not an array. Please enter an array of elements in `Cells / Column Values`.");
-    } else if (Array.isArray(cells[0])) {
-      throw new Error("Cell / Column data is a multi-dimensional array. A one-dimensional is expected. If you're trying to send multiple rows to Google Sheets, search for the action to add multiple rows to Sheets.");
-    }
-
-    const {
-      arr,
-      convertedIndexes,
-    } = this.googleSheets.arrayValuesToString(cells);
 
     const data = await this.googleSheets.addRowsToSheet({
       spreadsheetId: sheetId,
       range: this.sheetName,
-      rows: [
-        arr,
-      ],
+      rows: [Object.values(row)],
     });
 
-    let summary = `Added 1 row to [${this.sheetId?.label || this.sheetId} (${data.updatedRange})](https://docs.google.com/spreadsheets/d/${sheetId}).`;
-    if (convertedIndexes.length > 0) {
-      summary += " We detected something other than a string/number/boolean in at least one of the fields and automatically converted it to a string.";
-    }
-    $.export("$summary", summary);
+    $.export("$summary", `Added 1 row to [${this.sheetId?.label || this.sheetId} (${data.updatedRange})](https://docs.google.com/spreadsheets/d/${sheetId}).`);
 
     return data;
   },
